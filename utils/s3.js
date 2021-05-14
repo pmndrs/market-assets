139dfbbb2faf9c0c005317be94f9358bb2d6c4c9
Sara Vieira <hey@iamsaravieira.com>
2021-05-14 15:58:42 +0200

    rename env

--- utils/s3.js
+++ utils/s3.js
@@ -1,5 +1,13 @@
+require('dotenv').config()
 const aws = require('aws-sdk')
 const { S3Client } = require('@aws-sdk/client-s3')
 
 const spacesEndpoint = new aws.Endpoint('fra1.digitaloceanspaces.com')
-export const s3 = new S3Client({ endpoint: spacesEndpoint, region: 'fra1' })
+export const s3 = new S3Client({
+  endpoint: spacesEndpoint,
+  region: 'fra1',
+  credentials: {
+    accessKeyId: process.env.VERCEL_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.VERCEL_AWS_SECRET_ACCESS_KEY,
+  },
+})
