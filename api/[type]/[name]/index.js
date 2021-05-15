8874842d0e466394cb7c0c149d621ea1f071d12c
Sara Vieira <hey@iamsaravieira.com>
2021-05-15 22:36:26 +0200

    delete paths enpoints

--- api/[type]/[name]/index.js
+++ api/[type]/[name]/index.js
@@ -1,4 +1,3 @@
-import { getAllAssetLinks } from '../../../utils/endpoints/paths'
 import { getAllCategories } from '../../../utils/endpoints/categories'
 import { getCreator } from '../../../utils/endpoints/creator'
 import { getTeam } from '../../../utils/endpoints/team'
@@ -8,12 +7,9 @@ export default async function handler(req, res) {
   const assetType = req.query.type
   const name = req.query.name
 
-  if (name === 'paths') {
-    const paths = await getAllAssetLinks(assetType)
-    res.status(200).json(paths)
-  } else if (name === 'categories') {
-    const paths = await getAllCategories(assetType)
-    res.status(200).json(paths)
+  if (name === 'categories') {
+    const categories = await getAllCategories(assetType)
+    res.status(200).json(categories)
   } else if (assetType === 'creators') {
     const creator = await getCreator(name)
     res.status(200).json(creator)
